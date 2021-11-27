17285236ade7c061f2dcedb372e77f7f0560fe16
jenesius <lokargenia@gmail.com>
2021-11-27 16:08:48 +0300

    Update pushModal.ts, change return Modal to return Promise<Modal>

--- plugin/methods/addModal.ts
+++ plugin/methods/addModal.ts
@@ -3,16 +3,18 @@ import Modal from "../utils/Modal";
 import {state} from "../utils/state";
 import ModalError from "../utils/ModalError";
 
-export default function _addModal(component: any, params: any):Modal{
+/**
+ * СИНХРОННАЯ ФУНКЦИЯ ДЛЯ ДОБАВЛЕНИЯ КОМПОНЕНТЫ
+ * ПРОВЕРКА ИДЁТ ТОЛЬКО НА ВХОДНЫЕ ПАРАМЕТРЫ:
+ * - ИНИЦИАЛИЗАЦИЯ ПРИЛОЖЕНИЯ(СОЗДАНИЕ КОНТЕЙНЕРА В КОТОРОМ МОДАЛЬНЫЕ ОКНА ПОКАЗЫВАЮТСЯ)
+ * - ПЕРЕДАЧА КОМПОНЕНТЫ В КАЧЕСТВЕ ПАРАМЕТРА
+ * */
+export default function _addModal(component: any, params: any = {}):Modal{
 
 	if (!state.initialized) throw ModalError.NotInitialized();
-	
-	if (!component) throw ModalError.ModalComponentNotProvided();
-	
-	
+	if (!component) 		throw ModalError.ModalComponentNotProvided();
+
 	const modal = new Modal(component, params);
-	
 	modalQueue.value.push(modal);
-	
 	return modal;
 }
--- plugin/methods/pushModal.ts
+++ plugin/methods/pushModal.ts
@@ -3,7 +3,12 @@ import _addModal from "./addModal";
 import Modal from "../utils/Modal";
 /**
  * @description Method push modal to queue. Using this method you can open multiple windows. For closing use popModal
+ *
+ * @param {Object} component Any VueComponent
+ * @param {Object} props
+ *
+ * @return {Promise<Modal>} Modal
  * */
-export default function pushModal(component: any, props:any = {}):Modal {
-    return _addModal(component, props);
+export default function pushModal(component: any, props:any = {}):Promise<Modal> {
+    return Promise.resolve().then(() => _addModal(component, props))
 }

b4b92bc4e05a28463edb4701ee5448cf325b396b
jenesius <lokargenia@gmail.com>
2022-02-19 10:08:00 +0300

    Minimize routerIntegration

--- plugin/routerIntegration/index.ts
+++ plugin/routerIntegration/index.ts
@@ -55,8 +55,6 @@ import {RouteLocationNormalized, Router, RouteComponent} from "vue-router";
 import Modal from "../utils/Modal";
 import ModalError from "../utils/ModalError";
 
-
-
 interface ModalRouterInterface {
 	initialize(): Promise<void>,
 	_isModal: boolean,
@@ -86,36 +84,37 @@ function init(router: Router){
 	 * */
 	function findModal(routerLocation: RouteLocationNormalized): ModalRouterInterface | null{
 
-		if (!routerLocation.matched.length) return null;
-
 		for(let i = routerLocation.matched.length - 1; i >=0; i--) {
-
 			const components = routerLocation.matched[i].components;
-
+			/**
+			 * Problem:
+			 * Object.values(components)
+			 * return (RouteComponent | ModalRouterInterface)[]
+			 *
+			 * How to do it in TypeScript
+			 * */
 			// @ts-ignore
 			const a:ModalRouterInterface | null = Object.values(components).find((route: RouteComponent) => route._isModal);
 
 			if (a) return a;
 		}
-
 		return null;
-
 	}
 
 	/**
-	 * @description Hook only for closing
+	 * @description Hook only for closing #1
 	 * */
 	router.beforeEach(async (to:RouteLocationNormalized, from:RouteLocationNormalized) => {
 		try {
-			const modal = findModal(from);
-			if (modal && !modal.getModalObject()?.closed?.value) await modal.close(true);
+			const modalRoute = findModal(from);
+			if (modalRoute && !modalRoute.getModalObject()?.closed?.value) await modalRoute.close(true);
 		} catch (e){
 			return false;
 		}
 	})
 
 	/**
-	 * @description Hook for opening modal
+	 * @description Hook for opening modal #2
 	 * */
 	router.afterEach(async (to:any) => {
 		const modal:ModalRouterInterface | null = findModal(to);
@@ -132,6 +131,19 @@ function useModalRouter(component: any){
 
 	//Ссылка на modalObject
 	let modal: Modal | null			= null;
+
+	/**
+	 * isNavigationClosingGuard используется в качестве реле при закрытии модаль
+	 * ного окна. Т.к. мы подписываемся на закрытии модального окна и вызываем
+	 * переход на шаг назад в router, мы можем перейти сразу на два шага назад:
+	 * При управляемом переходе(при нажатии на предыдущую страницу, кнопку назад)
+	 * происходит сперва обычный переход на предыдущий route, а затем ещё один
+	 * переход назад: в onclose модального окна.
+	 *
+	 * Именно по этому, в onclose стоит проверка на isNavigationClosingGuard, а
+	 * в обработчике #1 передаётся true, указывающее на то, что при закрытии мод
+	 * ального кона не надо возвращаться на шаг назад в route.
+	 * */
 	let isNavigationClosingGuard	= false;
 
 	async function initialize(): Promise<void>{
@@ -149,6 +161,10 @@ function useModalRouter(component: any){
 
 	return{
 		getModalObject: () => modal,
+		/**
+		 Флаг, использующийся для определения того, что данная компонента -
+		 обёртка модального окна
+		 */
 		_isModal: true,
 
 		async close(v = false){
@@ -157,6 +173,13 @@ function useModalRouter(component: any){
 			if (modal) return await modal.close()
 		},
 		initialize,
+		/**
+		 * Мнимая обёртка. Для того, чтобы рендеринг запускался.
+		 * -----
+		 * (19.02.2022)
+		 * Try to change null to RouterView, using this way we can use children
+		 * in router configuration.
+		 * */
 		setup: () => () => null
 	};
 }

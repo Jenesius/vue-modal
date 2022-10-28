47c35fbb31d3f806c1c3b54913e8f06def944448
jenesius <lokargenia@gmail.com>
2022-10-28 15:01:16 +0300

    Add tests for promptModal

---
+++ tests/components/modal-prompt-value.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>{{value}}</p>
+</template>
+
+<script setup lang = "ts">
+import {defineEmits, defineProps, onMounted} from "vue";
+import Modal from "../../plugin/utils/Modal";
+
+interface IProps {
+  value: any,
+  timeout?: number
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  timeout: 1 * 1000
+})
+const emits = defineEmits<{
+  (event: typeof Modal.EVENT_PROMPT, value: any): void
+}>()
+
+
+onMounted(() => {
+  setTimeout(() => {
+    emits(Modal.EVENT_PROMPT, props.value);
+  })
+})
+
+</script>
---
+++ tests/prompt-modal.spec.ts
@@ -0,0 +1,27 @@
+import {mount} from "@vue/test-utils";
+import {container, promptModal} from "../plugin/index";
+import ModalPromptValue from "./components/modal-prompt-value.vue";
+import wait from "./wait";
+
+describe("Testing prompt-modal", () => {
+    it('Test for opened modal window', async function () {
+        const wrap = await mount(container);
+
+        promptModal(ModalPromptValue, {value: '123'})
+
+        await wait();
+
+        expect(wrap.text()).toEqual('123');
+    });
+    it("Should be returned with provided value", async function () {
+        await mount(container);
+
+        const value = Math.random();
+
+        expect(await promptModal(ModalPromptValue, {
+            value
+        })).toEqual(value)
+
+    })
+
+})

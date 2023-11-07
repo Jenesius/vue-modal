28b13b09208f83f744b64f954022e9d6a0c7eafa
jenesius <lokargenia@gmail.com>
2023-11-07 15:18:25 +0300

    example: use namespace

--- examples/use-namespace/App.vue
+++ examples/use-namespace/App.vue
@@ -1,10 +1,19 @@
 <template>
 	<div>
-		<input-field v-model = "message"/>
-		<button @click="open('Main')">Open Modal</button>
-		<button @click="push('hi', 'notification')">Push Notification</button>
+
+		<div class = "widget-card ">
+			<h2 class = "widget-card-full">Everything is modal </h2>
+			<input-field v-model = "message" class = "widget-card-full" label = "Write notification message"/>
+			<button @click="push('Main')">Default Namespace</button>
+			<button @click="push('hi', 'notification')">Push Notification</button>
+			<button @click="openForBottom">Namespace Bottom</button>
+		</div>
+
 		<modal-container/>
+
 		<notification-container/>
+		<modal-container namespace="bottom" />
+
 	</div>
 </template>
 
@@ -16,16 +25,29 @@ import pushModal from "@/methods/pushModal";
 import {ref} from "vue";
 import {InputField} from "jenesius-vue-form";
 
-const message = ref();
-function open(msg: string, namespace?: string) {
-	openModal(Modal, {msg}, {namespace})
-}
+const message = ref("You have new notification");
 function push(msg: string, namespace: string) {
 	pushModal(Modal, {msg: message.value}, {namespace})
 }
 
+function openForBottom() {
+	const msg = `This modal window is opened for namespace 'bottom'. Please note that the Esc handler does not work here.`;
+	pushModal(Modal, {msg}, {namespace: "bottom"})
+}
 
 </script>
 <style>
 @import "./../../project/default-style.css";
+
+.widget-card {
+	padding: 10px;
+	display: grid;
+	grid-template-rows: 65px 55px 35px;
+	grid-template-columns: repeat(3, 100px);
+	gap: 10px;
+}
+.widget-card-full {
+	grid-column: 1/4;
+}
+
 </style>

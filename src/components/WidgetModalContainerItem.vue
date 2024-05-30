e89627b1b1abd5b8186645c1ac25dcf4e19d3c7d
jenesius <lokargenia@gmail.com>
2024-05-30 10:16:10 +0300

    fix: move document ref to modalContainer ref.

--- src/components/WidgetModalContainerItem.vue
+++ src/components/WidgetModalContainerItem.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="widget__modal-container__item modal-container" @pointerdown.self.stop="handelClick">
+	<div class="widget__modal-container__item modal-container" @pointerdown.self.stop="handelClick" ref = "modalContainerRef">
 		<component
 			:is="modal.component"
 			v-bind="modal.props.value"
@@ -10,18 +10,20 @@
 		/>
 	</div>
 </template>
-<script setup>
+<script setup lang = "ts">
 import {onMounted, ref, watch} from "vue";
 import closeById from "../methods/closeById";
-import {getModalById} from "../utils/Modal";
+import Modal, {getModalById} from "../utils/Modal";
 import createDebug from "../utils/create-debug";
 
-const modalRef = ref(null);
 const debug = createDebug('modal-item')
-const props = defineProps({
-	id: Number,
-})
-const modal = getModalById(props.id);
+const props = defineProps<{
+	id: number
+}>()
+
+const modal = getModalById(props.id) as Modal;
+const modalContainerRef = ref<HTMLDivElement>()
+const modalRef = ref<unknown>();
 
 function handelClick() {
 	// if (e.target !== containerRef.value) return;
@@ -36,30 +38,40 @@ onMounted(() => {
 	debug(`Modal(${modal.id}) is mounted`);
 	
 	if (modal.draggable) {
-		
-		const draggableSelector = typeof modal.draggable === 'string' ? modal.draggable : `[modalid=_modal_${props.id}]`
+		const MODAL_QUERY_SELECTOR = `[modalid=_modal_${props.id}]`;
+		const draggableSelector = typeof modal.draggable === 'string' ? modal.draggable : MODAL_QUERY_SELECTOR
 		debug(`The modal window is movable(${draggableSelector}).`)
 		
-		setTimeout(() => {
-			addDraggable(document.querySelector(draggableSelector))
-		}, 0)
+		if (!modalContainerRef.value) {
+			debug(`The modal container is not mounted. Modal id %d.`, modal.id);
+		} else {
+			debug(`Finding an item to drag: %s`, draggableSelector);
+			addDraggable(
+				modalContainerRef.value?.querySelector(MODAL_QUERY_SELECTOR),
+				modalContainerRef.value.querySelector(draggableSelector)
+			)
+		}
 	}
 })
 
-function addDraggable(elementDraggable) {
+function addDraggable(elementModal: HTMLElement | null, elementDraggable: HTMLElement | null) {
 	if (!elementDraggable) {
 		debug('draggable item not found.')
 		return;
 	}
-	const elementModal = document.querySelector(`[modalid=_modal_${props.id}]`);
+	
+	if (!elementModal) {
+		debug('HTML modal is not founded.')
+		return;
+	}
 	
 	elementDraggable.addEventListener('pointerdown', (event) => {
 		const currentStyleValues = elementModal.getAttribute('style')?.match(/transform: translate\((.*)px,\s*(.*)px\)/);
-		let shiftX = currentStyleValues?.[1];
-		let shiftY = currentStyleValues?.[2]
+		const initialX = currentStyleValues?.[1];
+		const initialY = currentStyleValues?.[2];
 		
-		shiftX = typeof shiftX === 'string' ? Number.parseInt(shiftX) : 0;
-		shiftY = typeof shiftY === 'string' ? Number.parseInt(shiftY) : 0;
+		let shiftX = typeof initialX === 'string' ? Number.parseInt(initialX) : 0;
+		let shiftY = typeof initialY === 'string' ? Number.parseInt(initialY) : 0;
 		
 		const {clientX, clientY} = event;
 		const startX = clientX;
@@ -70,7 +82,7 @@ function addDraggable(elementDraggable) {
 		document.addEventListener('pointermove', handleDraggableMove, { passive: true })
 		document.addEventListener('pointerup', handleDraggableEnd, {once: true})
 		
-		function handleDraggableMove(event) {
+		function handleDraggableMove(event: PointerEvent) {
 			const {clientX, clientY} = event;
 			
 			const resultX = shiftX + clientX - startX;
@@ -78,9 +90,9 @@ function addDraggable(elementDraggable) {
 			
 			debug(`move at (${resultX}, ${resultY})`)
 			
-			elementModal.style.transform = `translate(${resultX}px,${resultY}px)`
+			elementModal!.style.transform = `translate(${resultX}px,${resultY}px)`
 		}
-		function handleDraggableEnd(event) {
+		function handleDraggableEnd(event: PointerEvent) {
 			debug(`movement completed`)
 			document.removeEventListener('pointermove', handleDraggableMove)
 			event.preventDefault();
